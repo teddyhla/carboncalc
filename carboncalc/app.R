f512f05a68168a84ffd5690852f6c8e4add9731d
teddyhla <teddyhla@gmail.com>
2023-05-18 10:27:35 +0100

    first draft

--- carboncalc/app.R
+++ carboncalc/app.R
@@ -2,27 +2,56 @@
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
 #
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# Conceived by A Wong, M Zawadka and T Hla
+# see github repo for details 
 
+# DEPENDENCIES 
 library(shiny)
+library(shinythemes)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <- fluidPage(theme = shinytheme("lumen"),
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Carbon cost of Local vs. International POCUS courses"),
+    
+    #horizontal panel
+    hr(),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
+        sidebarPanel("Please select variables",
+                br(),
+                     br(),
+            sliderInput("attnd",
+                        "Variable 1: Number of attendees:",
                         min = 1,
-                        max = 50,
-                        value = 30)
+                        max = 200,
+                        value = 30),
+                br(),
+            sliderInput("fac",
+                        "Variable 2: Number of faculty:",
+                        min = 1,
+                        max = 20,
+                        value = 5),
+            br(),
+            sliderInput("p_attnd",
+                        "Variable 3: Percentage of international attendees",
+                        min = 0,
+                        max = 100,
+                        value = 50),
+            br(),
+            sliderInput("fac_attnd",
+                        "Variable 4: Percentage of international faculty",
+                        min = 0,
+                        max = 100,
+                        value = 50),
+            numericInput("mu_loc","Variable 5: Mean distance for local in km",
+                         value = 50, min = 5, max = 100,step = 5),
+            br(),
+            numericInput("mu_int","Variable 6: Mean distance for international in km",
+                         value = 500, min = 150, max = 1500,step = 50)
+                
         ),
 
         # Show a plot of the generated distribution
---
+++ readme.md
@@ -0,0 +1,6 @@
+
+
+
+## Session Info
+
+R version 4.2.1 (2022-06-23)Platform: x86_64-apple-darwin17.0 (64-bit)Running under: macOS Monterey 12.6.5Matrix products: defaultLAPACK: /Library/Frameworks/R.framework/Versions/4.2/Resources/lib/libRlapack.dyliblocale:[1] en_US.UTF-8/en_US.UTF-8/en_US.UTF-8/C/en_US.UTF-8/en_US.UTF-8attached base packages:[1] stats     graphics  grDevices utils     datasets  methods  [7] base     loaded via a namespace (and not attached):[1] compiler_4.2.1   rsconnect_0.8.29 tools_4.2.1     [4] rstudioapi_0.14  curl_5.0.0       jsonlite_1.8.4  [7] digest_0.6.31    openssl_2.0.6    askpass_1.1    
